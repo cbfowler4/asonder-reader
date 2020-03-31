f0adb33347c7089a6548a4dac0a19a2154a61885
Bryan <cbfowler4@gmail.com>
2020-03-31 11:50:32 -0400

    navigates to builder properly

--- src/components/reader.js
+++ src/components/reader.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { PRODUCT_QS_NAME, VALID_ATTR_LABELS } from '../helpers/configs';
 
+const isLocal = () => window.location.href.includes('localhost');
+
+const navigateToBuilder = (isEditing) => {
+  if (isLocal()) return;
+  const { origin, pathname, search } = window.location;
+  const splitPath = pathname.split('/');
+  const productsIdx = splitPath.findIndex((name) => name === 'products');
+  const productName = splitPath[productsIdx + 1];
+
+  let newSearch = `${PRODUCT_QS_NAME}=${productName}`;
+  if (isEditing) {
+    const urlParams = new URLSearchParams(search);
+    urlParams.set(PRODUCT_QS_NAME, productName);
+    newSearch = urlParams.toString();
+  }
+
+  window.location.assign(`${origin}/pages/builder?${newSearch}`);
+}
+
 const ReaderDisplay = ({ selections }) => (
   <div className='reader-display'>
     {
       selections.map(({ label, value }) => (
         <div key={ label } className='selection-row'>
-          <h2>{`${label}: ${value}`}</h2>
+          <span className='label'>{`${label}: `}</span>
+          <span className='value'>{ value }</span>
           <input
               type='radio'
               name={ `properties[${label}]` }
@@ -17,6 +37,21 @@ const ReaderDisplay = ({ selections }) => (
         </div>
       ))
     }
+    <div
+      className='edit-link'
+      onClick={ () => { navigateToBuilder(true); } }
+    >
+      Change
+    </div>
+    <button
+      type='submit'
+      name='add'
+      class='btn btn--add-to-cart'>
+      <span class="btn__text">
+        Add To Cart
+      </span>
+    </button>
+    
   </div>
 );
 
@@ -25,15 +60,16 @@ export const Reader = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const selections = [];
 
-  if (urlParams.get(PRODUCT_QS_NAME)) {
-    
+  if (urlParams.get(PRODUCT_QS_NAME)) {    
     urlParams.forEach((value, label) => {
       console.log(value, label)
-      if (VALID_ATTR_LABELS[label]) {
+      if (VALID_ATTR_LABELS[label.toLowerCase()] && value) {
         selections.push({ label, value });
       }
     });
+  }
 
+  if (selections.length !== 0) {
     return (
       <div className='reader-container'>
         <ReaderDisplay selections={ selections } />
@@ -43,7 +79,10 @@ export const Reader = () => {
 
   return (
     <div className='reader-container'>
-      <div className='btn btn--add-to-cart'>
+      <div
+        className='btn btn--add-to-cart'
+        onClick={ () => navigateToBuilder() }
+      >
         <span className='btn__text'>
           Customize
         </span>
--- src/helpers/configs.js
+++ src/helpers/configs.js
@@ -1,8 +1,8 @@
 export const PRODUCT_QS_NAME = 'product';
 
 export const VALID_ATTR_LABELS = {
-  'Mouthpiece': true,
-  'Bowl': true,
-  'Custom Text': true,
-  'Material': true,
+  'mouthpiece': true,
+  'bowl': true,
+  'custom text': true,
+  'material': true,
 }
